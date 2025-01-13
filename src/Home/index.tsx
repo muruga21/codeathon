d6e470c93a8728714285df238234a666136a58b0
muruga21 <murugaperumal21062004@gmail.com>
2025-01-13 12:56:56 +0530

    add: added base template for the site

---
+++ src/Home/index.tsx
@@ -0,0 +1,15 @@
+import "./Style.css"
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
+
+const Home = () => {
+  return (
+    <Stack spacing={2} direction="row">
+      <Button variant="text">Text</Button>
+      <Button variant="contained">Contained</Button>
+      <Button variant="outlined">Outlined</Button>
+    </Stack>
+  )
+}
+
+export default Home
