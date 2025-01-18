4af633151595b378556ca4d61c484b88c52ba108
muruga21 <murugaperumal21062004@gmail.com>
2025-01-18 22:02:17 +0530

    resovled ourteam

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import './App.css'
+import Contributors from './Contributors/Contributors'
 import Home from './Home'
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
   return (
     <>
       <Home/>
+      <Contributors/>
     </>
   )
 }
