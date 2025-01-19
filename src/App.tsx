9b1b21ef77df9085d76518976f6566b44b7f6c2a
pavithrab30 <pavithra.b2023ece@sece.ac.in>
2025-01-19 10:32:47 +0530

    don

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,25 @@
-import './App.css'
-import Home from './Home'
+import React from 'react';
+import './App.css'; // Assuming you have a CSS file for global styles
+import Contributors from './components/Contributors'; // Correct path to Contributors component
+import OurTeam from './components/OurTeam';  // Correct path to OurTeam component
 
-function App() {
-
+const App: React.FC = () => {
   return (
-    <>
-      <Home/>
-    </>
-  )
+    <div className="App">
+     
+      
+      <div className="section my-8 text-center">
+        <h2 className="text-4xl mb-8 font-bold ">Contributors</h2>
+        <Contributors /> 
+      </div>
+
+
+      <div className="section my-8 text-center">
+        <h2 className="text-4xl mb-8 font-bold">Our Team</h2>
+        <OurTeam /> 
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/ourteam.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import './ourteam.css'; // Ensure the CSS file is linked correctly
+import { FaLinkedin } from 'react-icons/fa';
+
+const OurTeam = () => {
+  const teamMembers = [
+    {
+      name: 'Griffin Annshual',
+      img: 'https://res.cloudinary.com/startup-grind/image/upload/c_fill,w_250,h_250,g_center/c_fill,dpr_2.0,f_auto,g_center,q_auto:good/v1/gcs/platform-data-goog/avatars/griffin_annshual_PCPNBuc.jpg',
+      linkedin: 'https://www.linkedin.com/in/griffin-annshual-97b615232/',
+      domain: 'GDG',
+      role: 'Co-Organizer',
+    },
+  ];
+
+  return (
+    <div className="ourteam-page">
+      <h1 className="ourteam-heading"></h1>
+      <div className="ourteam-container">
+        {teamMembers.map((member, index) => (
+          <div className="card" key={index}>
+            <div className="card-content">
+              <img
+                src={member.img}
+                alt={member.name}
+                className="card-img"
+              />
+              <div className="heading">{member.name}</div>
+              <p>{member.domain}</p>
+              <p>{member.role}</p>
+              <a
+                href={member.linkedin}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <div className="linkedin-icon">
+                  <FaLinkedin />
+                </div>
+              </a>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default OurTeam;
